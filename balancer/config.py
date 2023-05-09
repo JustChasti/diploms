1aabdf23ea13c389144f2670098e18ff7aea0263
JustChasti <chastytim@mail.ru>
2023-05-09 18:19:11 +0300

    add proxy to project

--- balancer/config.py
+++ balancer/config.py
@@ -1,12 +1,30 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 admin_password = 'Qwerty21'
 my_host = '0.0.0.0'
 base_host = 'mongo'
 base_port = 27017
 client_name = 'balancer'
+login = os.getenv('LOGIN')
+password = os.getenv('PASSWORD')
 
+proxy_list = {
+    '185.231.244.160',
+    '185.231.244.178',
+    '185.231.244.69',
+    '185.231.244.181',
+    '185.231.244.222',
+    '185.231.244.132',
+    '185.231.244.142',
+    '185.231.244.141',
+    '185.231.244.143',
+    '185.231.244.146'
+}
+
+http_port = 59100
 rabbit_host = 'rabbitmq'
 queue_tasks_name = 'tasks'
 
