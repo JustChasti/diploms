3d33c8275f118f57782ce9b7112a180834d8cfae
JustChasti <chastytim@mail.ru>
2023-05-14 18:16:05 +0300

    add main page

--- web_client/views/main.py
+++ web_client/views/main.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from config import my_host
 
 
 main_router = APIRouter()
+main_router.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -12,5 +15,13 @@ templates = Jinja2Templates(directory="templates")
 async def main(request: Request):
     return templates.TemplateResponse(
         "main.html",
-        {"request": request}
+        {
+            "request": request, 
+            "home": "/", 
+            "api": "/api",
+            "pricing": "/pricing",
+            "about": "/about",
+            "login": "/login",
+            "signup": "/signup",
+        }
     )
