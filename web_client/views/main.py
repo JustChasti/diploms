c872ee04217f8f697510bb4d85e3ad7bf1d2b549
JustChasti <chastytim@mail.ru>
2023-05-16 15:30:56 +0300

    add some pages

--- web_client/views/main.py
+++ web_client/views/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from config import my_host
@@ -44,8 +44,66 @@ async def login(request: Request):
     )
 
 
+@main_router.get('/about')
+async def login(request: Request):
+    return templates.TemplateResponse(
+        "about.html",
+        {
+            "request": request, 
+            "home": "/", 
+            "api": "/api",
+            "pricing": "/pricing",
+            "about": "/about",
+            "login": "/login",
+            "signup": "/signup",
+        }
+    )
+
+
+@main_router.get('/signup')
+async def signup(request: Request):
+    return templates.TemplateResponse(
+        "registration.html",
+        {
+            "request": request, 
+            "home": "/", 
+            "api": "/api",
+            "pricing": "/pricing",
+            "about": "/about",
+            "login": "/login",
+            "signup": "/signup",
+        }
+    )
+
+
 @main_router.post('/login')
 async def login_analys(request: Request):
     form = LoginForm(request)
     await form.load_data()
-    return JSONResponse({'login': form.login, 'password': form.password})
+    if form.login == 'chastytim@mail.ru':
+        return RedirectResponse('/api')
+    return templates.TemplateResponse(
+        "error.html",
+        {
+            "request": request, 
+            "error_name": "Ошибка 0", 
+            "error_text": "Текст ошибки 0",
+        }
+    )
+
+
+@main_router.post('/signup')
+async def signup_analys(request: Request):
+    form = LoginForm(request)
+    await form.load_data()
+    if form.login == 'chastytim@mail.ru':
+        return RedirectResponse('/api')
+    return templates.TemplateResponse(
+        "error.html",
+        {
+            "request": request, 
+            "error_name": "Ошибка 0", 
+            "error_text": "Текст ошибки 0",
+        }
+    )
+
