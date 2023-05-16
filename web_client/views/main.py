e987d6c8a27e1d8100fb17e5d4ebcf68f1c1b444
JustChasti <chastytim@mail.ru>
2023-05-16 19:02:29 +0300

    create all pages

--- web_client/views/main.py
+++ web_client/views/main.py
@@ -76,18 +76,62 @@ async def signup(request: Request):
     )
 
 
+@main_router.get('/api')
+async def signup(request: Request):
+    access_token = request.cookies.get('access_token')
+    refresh_token = request.cookies.get('refresh_token')
+    # здесь отправляем запрос на получение пользователя
+    # если он пишет что токен истек обновляем acces_token
+    if access_token and refresh_token:
+        user_login = 'chastytim@mail.ru'
+        user_id = 'abc1234dd'
+        tasks = 3
+        proxies = 5
+        proxies_max = 20
+        return templates.TemplateResponse(
+            "api.html",
+            {
+                "request": request, 
+                "home": "/", 
+                "api": "/api",
+                "pricing": "/pricing",
+                "about": "/about",
+                "login": "/login",
+                "signup": "/signup",
+                "user_login": user_login,
+                "user_id": user_id,
+                "tasks": tasks,
+                "proxies": proxies,
+                "proxies_max": proxies_max
+            }
+        )
+    else:
+        return templates.TemplateResponse(
+            "error.html",
+            {
+                "request": request, 
+                "error_name": "Ошибка авторизации", 
+                "error_text": "Вы не авторизованы, войдите в систему",
+            }
+        )
+
+
+
 @main_router.post('/login')
 async def login_analys(request: Request):
     form = LoginForm(request)
     await form.load_data()
-    if form.login == 'chastytim@mail.ru':
-        return RedirectResponse('/api')
+    if form.login == 'chastytim@mail.ru':  # тут идет запрос к api
+        response = RedirectResponse('/api', 303)
+        response.set_cookie(key="access_token", value="auth - jwt token")
+        response.set_cookie(key="refresh_token", value="refr - jwt token")
+        return response
     return templates.TemplateResponse(
         "error.html",
         {
             "request": request, 
-            "error_name": "Ошибка 0", 
-            "error_text": "Текст ошибки 0",
+            "error_name": "Ошибка Авторизации", 
+            "error_text": "Вы не авторизованы",
         }
     )
 
@@ -96,8 +140,11 @@ async def login_analys(request: Request):
 async def signup_analys(request: Request):
     form = LoginForm(request)
     await form.load_data()
-    if form.login == 'chastytim@mail.ru':
-        return RedirectResponse('/api')
+    if form.login == 'chastytim@mail.ru':  # тут идет запрос к api
+        response = RedirectResponse('/api', 303)
+        response.set_cookie(key="auth_token", value="auth - jwt token")
+        response.set_cookie(key="refr_token", value="refr - jwt token")
+        return response
     return templates.TemplateResponse(
         "error.html",
         {
